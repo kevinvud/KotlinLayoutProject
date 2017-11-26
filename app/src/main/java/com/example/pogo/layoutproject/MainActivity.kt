c040032f84f69d007d8a5522aba8f2b78cea5c53
kevinvud <kevinvud@gmail.com>
2017-11-25 21:00:33 -0800

    Done with portrait layout on phone and tablet

--- app/src/main/java/com/example/pogo/layoutproject/MainActivity.kt
+++ app/src/main/java/com/example/pogo/layoutproject/MainActivity.kt
@@ -16,5 +16,7 @@ class MainActivity : AppCompatActivity() {
         val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
         rounded.cornerRadius = 15f
         logo.setImageDrawable(rounded)
+
+
     }
 }
